54c0c73c2af38c6c00ac3679e700653606ca99a6
Madhan Thiyagarajan <40027485+flyingorange7@users.noreply.github.com>
2022-10-17 22:23:23 -0400

    Time: 1 ms, Memory: 42.7 MB - LeetHub

---
+++ implement-strstr/implement-strstr.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        for(int i = 0; i < haystack.length(); i++){
+            boolean match = true;
+            for(int j = 0; j < needle.length(); j++){
+                if(i+j >= haystack.length())
+                    return -1;
+                if(haystack.charAt(i+j) != needle.charAt(j)){
+                    match = false;
+                    break;
+                }
+            }
+            if(match)
+                return i;
+        }
+        return -1;
+    }
+}
